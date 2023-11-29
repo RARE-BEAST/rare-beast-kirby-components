2cecf0e38dd5962fae5e89a4bf582699f131fb0f
Erik Pedersen <109049703+basementlivin@users.noreply.github.com>
2023-11-29 17:41:10 -0500

    Complete basic SCSS structure

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -1,3 +1,22 @@
-<!-- Kirby will automatically load this template for the Projects page -->
-<!-- Why? Because the names match, baby! -->
+<?php snippet('header') ?>
 
+<main class="main">
+    <h1 class="page-title"><?= $page->title() ?></h1>
+    
+    <ul class="projects">
+
+        <?php foreach ($page->children()->listed() as $project): ?>
+            
+            <li class="project">
+                <a href="<?= $project->url() ?>" class="project__url">
+                    <?= $project->title() ?>
+                </a>
+            </li>
+        
+        <?php endforeach; ?>
+
+    </ul>
+
+</main>
+
+<?php snippet('footer') ?>
