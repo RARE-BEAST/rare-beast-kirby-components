2adeed58b90e3e10dd164d08c0050951c4b11e1d
Erik Pedersen <109049703+basementlivin@users.noreply.github.com>
2023-12-14 11:53:01 -0500

    Add custom Panel options

--- site/config/config.php
+++ site/config/config.php
@@ -1,5 +1,45 @@
 <?php
 
+
+// SET THIS TO FALSE FOR PRODUCTION
 return [
     'debug' => true
 ];
+
+
+// OPTION TO ADD CUSTOM FAVICONS
+return [
+    'panel' => [
+      'favicon' => [
+        'apple-touch-icon' => [
+          'type' => 'image/png',
+          'url'  =>  'assets/apple-touch-icon.png',
+        ],
+        'shortcut icon' => [
+          'type' => 'image/svg+xml',
+          'url'  => 'assets/favicon.svg',
+        ],
+        'alternate icon' => [
+          'type' => 'image/png',
+          'url'  => 'assets/favicon.png',
+        ]
+      ]
+    ]
+  ];
+
+
+// OPTION TO MOVE THE PANEL TO A DIFFERENT URL
+// FOR SECURITY, BUT ALSO FOR FUN OR BRANDING PURPOSES
+return [
+    'panel' => [
+      'slug' => 'super-secret-admin-area'
+    ]
+  ];
+
+
+// OPTION TO ADD CUSTOM CSS TO THE PANEL
+// return [
+//     'panel' => [
+//       'css' => 'assets/css/custom-panel.css'
+//     ]
+//   ];
