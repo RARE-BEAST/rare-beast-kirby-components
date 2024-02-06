88226007e1243a7ce5ff9d8beb34e9d8c8c81d7f
Erik Pedersen <109049703+basementlivin@users.noreply.github.com>
2024-02-06 17:43:44 -0500

    Create basic 404 template

---
+++ site/templates/error.php
@@ -0,0 +1,15 @@
+<?php snippet('head') ?>
+<?php snippet('header') ?>
+
+<main class="main" id="main">
+    <section class="error wrapper wrapper--narrow">
+        <div class="error__inner">
+
+            <h1 class="h0">We can't find what you're looking for.</h1>
+            <a class="btn btn--primary" href="<?= $site->url() ?>">Back Home</a>
+        
+        </div>
+    </section>     
+</main>
+
+<?php snippet('footer') ?>
